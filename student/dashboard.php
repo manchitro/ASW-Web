65976ce58c663c72b90714d12393cedeef9a699e
Sazid <initialsaremsu@gmail.com>
2020-03-24 00:02:02 +0600

    Minor change

--- student/dashboard.php
+++ student/dashboard.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 if (isset($_SESSION['userAcademicId']) && $_SESSION['userAcademicId']!== "") {
+	echo $_SESSION['userAcademicId']." ".$_SESSION['userFullName'];
 }
 else{
 	header("Location: ../login.php?error=nosession");
