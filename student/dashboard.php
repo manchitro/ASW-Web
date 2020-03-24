3690de570422d7192fd9f81c17fbaf9209d00ede
Md. Pial Hasan <hasanmohammadpial@gmail.com>
2020-03-24 21:45:15 +0600

    added student dashboard html

--- student/dashboard.php
+++ student/dashboard.php
@@ -1,11 +1,29 @@
 <?php
-session_start();
-if (isset($_SESSION['userAcademicId']) && $_SESSION['userAcademicId']!== "" && isset($_SESSION['userFullName']) && $_SESSION['userFullName']!== "") {
-	echo $_SESSION['userAcademicId']." ".$_SESSION['userFullName'];
-}
-else{
-	header("Location: ../login.php?error=nosession");
-	exit();
-}
+//session_start();
+//if (isset($_SESSION['userAcademicId']) && $_SESSION['userAcademicId']!== "" && isset($_SESSION['userFullName']) && $_SESSION['userFullName']!== "") {
+//	echo $_SESSION['userAcademicId']." ".$_SESSION['userFullName'];
+//}
+//else{
+//	header("Location: ../login.php?error=nosession");
+//	exit();
+//}
 ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Student - Dashboard</title>
+</head>
+<body>
+	<header>
+		<div>
+			<p>Welcome, Student</p>
+			<p id= >xx-xxxxx-x</p>
+		</div>
+	</header>
+	<div>
+		<button type="submit" name="scan">Scan</button>
+	<button type="submit" name="history">History</button>
+	</div>
+</body>
+</html>
