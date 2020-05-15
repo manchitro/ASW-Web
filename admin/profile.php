8ca6532cd2f1de6b794218fb35e5dd65dbb4592e
AsirIntesar <62045229+AsirIntesar@users.noreply.github.com>
2020-05-14 19:24:21 -0700

    log out and password change added
    admin can log out now. and admin can change his/her password by thyself. front end has been created for the changing password option.

--- admin/profile.php
+++ admin/profile.php
@@ -13,16 +13,29 @@ else{
 <head>
   <title>Admin Dashboard</title>
   <link rel="icon" href="favicon.png">
+
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 </head>
 
-<body>
+<body class="p-3" style="background-color:#a5c0d2;">
 
-  <h1 >Admin Dashboard</h1>
+  <div class="d-flex justify-content-between">
+    <h1 style="color:red;" >Admin Dashboard</h1>
+    
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+      Change Password
+    </button>
+
+    <a class="btn btn-danger" href="../logout.php">Log Out</a>
+  </div>
 
   <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method ="POST">
     <textarea name = "qbox" rows = "10" cols = "50" value = "" placeholder="Write query here"></textarea><br>
 
-    <input type="submit" value="submit"><br>
+    <input type="submit" value="Submit">
+
   </form>
   <br><br>
 
@@ -31,6 +44,51 @@ else{
   include 'includes/queryprocess.inc.php'
   ?>
 
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Change Admin Password</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+         
+          <div class="modal-body">
+            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+              <div class="form-group row">
+                <label for="oldPassword" class="col-sm-2 col-form-label">Old Password</label>
+                <div class="col-sm-10">
+                  <input type="password" class="form-control" id="oldPassword" name="oldPassword" required="">
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="newPassword" class="col-sm-2 col-form-label">New Password</label>
+                <div class="col-sm-10">
+                  <input type="password" class="form-control" id="newPassword" name="newPassword" required="">
+                </div>
+              </div>
+              <div class="form-group row">
+                <div class="col-sm-10 offset-2">
+                  <button type="submit" class="btn btn-primary">Save Changes</button>
+                </div>
+              </div>
+            </form>
+          </div>
+        
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+	<!-- Bootstrap js -->
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 
 </body>
 </html>
---
+++ aswdb-1.sql
@@ -0,0 +1,332 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 11, 2020 at 06:45 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `aswdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendances`
+--
+
+CREATE TABLE `attendances` (
+  `Id` int(11) NOT NULL,
+  `StudentId` int(11) NOT NULL,
+  `ClassId` int(11) NOT NULL,
+  `Entry` int(11) NOT NULL,
+  `ScanTime` datetime NOT NULL,
+  `CreatedAt` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `classes`
+--
+
+CREATE TABLE `classes` (
+  `Id` int(11) NOT NULL,
+  `SectionId` int(11) NOT NULL,
+  `ClassDate` date NOT NULL,
+  `ClassType` int(11) NOT NULL,
+  `StartTimeId` int(11) NOT NULL,
+  `EndTimeId` int(11) NOT NULL,
+  `RoomNo` text NOT NULL,
+  `QRCode` text NOT NULL,
+  `QRDisplayStartTime` datetime NOT NULL,
+  `QRDisplayEndTime` datetime NOT NULL,
+  `CreatedAt` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `classtimes`
+--
+
+CREATE TABLE `classtimes` (
+  `Id` int(11) NOT NULL,
+  `ClassTimeText` text NOT NULL,
+  `CreatedAt` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `classtimes`
+--
+
+INSERT INTO `classtimes` (`Id`, `ClassTimeText`, `CreatedAt`) VALUES
+(0, '8:00', '2020-03-09 13:44:25'),
+(1, '8:30', '2020-03-09 13:45:04'),
+(2, '9:00', '2020-03-09 13:47:38'),
+(3, '9:30', '2020-03-09 13:47:38'),
+(4, '10:00', '2020-03-09 13:47:38'),
+(5, '10:30', '2020-03-09 13:47:38'),
+(6, '11:00', '2020-03-09 13:47:38'),
+(7, '11:30', '2020-03-09 13:47:38'),
+(8, '12:00', '2020-03-09 13:47:38'),
+(9, '12:30', '2020-03-09 13:47:38'),
+(10, '1:00', '2020-03-09 13:47:38'),
+(11, '1:30', '2020-03-09 13:47:38'),
+(12, '2:00', '2020-03-09 13:47:38'),
+(13, '2:30', '2020-03-09 13:47:38'),
+(14, '3:00', '2020-03-09 13:47:38'),
+(15, '3:30', '2020-03-09 13:47:38'),
+(16, '4:00', '2020-03-09 13:47:38'),
+(17, '4:30', '2020-03-09 13:47:38'),
+(18, '5:00', '2020-03-09 13:47:38'),
+(19, '5:30', '2020-03-09 13:47:38'),
+(20, '6:00', '2020-03-09 13:47:38'),
+(21, '6:30', '2020-03-09 13:47:38'),
+(22, '7:00', '2020-03-09 13:47:38'),
+(23, '7:30', '2020-03-09 13:47:38'),
+(24, '8:00', '2020-03-09 13:47:38');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sections`
+--
+
+CREATE TABLE `sections` (
+  `Id` int(11) NOT NULL,
+  `SectionName` text NOT NULL,
+  `FacultyId` int(11) NOT NULL,
+  `CreatedAt` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `sections`
+--
+
+INSERT INTO `sections` (`Id`, `SectionName`, `FacultyId`, `CreatedAt`) VALUES
+(28, 'Web Tech [C]', 7, '2020-04-02 22:48:34'),
+(29, 'Web Tech [B]', 7, '2020-04-02 22:49:06'),
+(30, 'Java [D]', 7, '2020-04-02 23:54:23'),
+(32, 'AC Lab [K]', 7, '2020-04-09 19:12:33'),
+(40, 'dawuyegf[S]', 5, '2020-05-04 20:08:59');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sectionstudents`
+--
+
+CREATE TABLE `sectionstudents` (
+  `SectionId` int(11) NOT NULL,
+  `StudentId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `sectionstudents`
+--
+
+INSERT INTO `sectionstudents` (`SectionId`, `StudentId`) VALUES
+(29, 9);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sectiontimes`
+--
+
+CREATE TABLE `sectiontimes` (
+  `Id` int(11) NOT NULL,
+  `StartTimeId` int(11) NOT NULL,
+  `EndTimeId` int(11) NOT NULL,
+  `WeekDayId` int(11) NOT NULL,
+  `ClassType` int(11) NOT NULL,
+  `RoomNo` text NOT NULL,
+  `SectionId` int(11) NOT NULL,
+  `CreatedAt` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `sectiontimes`
+--
+
+INSERT INTO `sectiontimes` (`Id`, `StartTimeId`, `EndTimeId`, `WeekDayId`, `ClassType`, `RoomNo`, `SectionId`, `CreatedAt`) VALUES
+(11, 0, 4, 0, 1, 'D0503', 28, '2020-04-02 22:48:34'),
+(12, 6, 12, 2, 0, 'D0204', 28, '2020-04-02 22:48:34'),
+(13, 0, 4, 1, 1, 'D0503', 29, '2020-04-02 22:49:06'),
+(14, 0, 6, 3, 0, 'D0203', 29, '2020-04-02 22:49:06'),
+(15, 0, 6, 0, 0, 'D0202', 30, '2020-04-02 23:54:23'),
+(16, 0, 4, 2, 1, '3107', 30, '2020-04-02 23:54:23'),
+(18, 0, 6, 4, 0, '3113', 32, '2020-04-09 19:12:33'),
+(19, 0, 4, 0, 0, '1115', 33, '2020-05-04 14:53:52'),
+(20, 0, 4, 0, 0, '1115', 34, '2020-05-04 14:53:59'),
+(21, 0, 4, 0, 0, '1115', 35, '2020-05-04 14:54:06'),
+(22, 0, 4, 0, 0, '1115', 36, '2020-05-04 14:54:13'),
+(23, 0, 4, 2, 0, '1115', 36, '2020-05-04 14:54:13'),
+(24, 0, 4, 0, 0, '1116', 37, '2020-05-04 15:39:27'),
+(25, 0, 4, 0, 0, '1116', 38, '2020-05-04 15:44:17'),
+(26, 0, 4, 0, 0, '1116', 39, '2020-05-04 20:05:45'),
+(27, 0, 4, 0, 0, '1115', 40, '2020-05-04 20:08:59'),
+(28, 0, 4, 0, 1, '1115', 41, '2020-05-04 20:11:50'),
+(29, 0, 6, 2, 0, '1111', 41, '2020-05-04 20:11:50');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `Id` int(11) NOT NULL,
+  `AcademicId` text NOT NULL,
+  `FirstName` text NOT NULL,
+  `LastName` text NOT NULL,
+  `Email` text DEFAULT NULL,
+  `Password` text DEFAULT NULL,
+  `UserType` int(11) NOT NULL,
+  `CreatedAt` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`Id`, `AcademicId`, `FirstName`, `LastName`, `Email`, `Password`, `UserType`, `CreatedAt`) VALUES
+(5, '1111-1111-1', 'Faculty', 'One', 'f1@aiub.edu', '$2y$10$.afGlGQoL0X6yeyVmR2qSe/YJCNCc6a8QwdBHcdPKx/bwXI6TMLXC', 0, '2020-03-16 08:35:33'),
+(7, '2222-2222-2', 'Faculty', 'Two', 'f2@aiub.edu', '$2y$10$eWLdrq/OcMXguN.oGz.HFu9TK2WojUiP5Ngt23.kD.X53MJbWkSj2', 0, '2020-03-17 20:28:02'),
+(8, '1', 'Admin', 'One', 'admin', '$2y$10$snPCbKNuHwSIzmGUR0O6IeWFEPm79KOqb/.rixK.f43R04KGjLdcC', 0, '2020-04-09 10:52:09'),
+(9, '18-37712-1', 'MD. SAZID', 'UDDIN', NULL, NULL, 1, '2020-05-02 22:56:54'),
+(10, '19-33142-1', 'FIWB', 'BGWEU', NULL, NULL, 1, '2020-05-03 00:30:59'),
+(11, '18-13242-1', 'JAKIA', 'RAHMAN', NULL, NULL, 1, '2020-05-03 01:00:34'),
+(12, '19-41244-1', 'ANJUMAN', 'ARA    ', NULL, NULL, 1, '2020-05-03 01:01:42'),
+(13, '20-24124-1', 'FNJWAHB', 'HJBFJH', NULL, NULL, 1, '2020-05-03 14:03:12');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `weekdays`
+--
+
+CREATE TABLE `weekdays` (
+  `Id` int(11) NOT NULL,
+  `WeekDayText` text NOT NULL,
+  `CreatedAt` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `weekdays`
+--
+
+INSERT INTO `weekdays` (`Id`, `WeekDayText`, `CreatedAt`) VALUES
+(0, 'Sunday', '2020-03-09 13:42:40'),
+(1, 'Monday', '2020-03-09 13:43:49'),
+(2, 'Tuesday', '2020-03-09 13:43:49'),
+(3, 'Wednesday', '2020-03-09 13:43:49'),
+(4, 'Thursday', '2020-03-09 13:43:49'),
+(5, 'Friday', '2020-03-09 13:43:49'),
+(6, 'Saturday', '2020-03-09 13:43:49');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `attendances`
+--
+ALTER TABLE `attendances`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indexes for table `classes`
+--
+ALTER TABLE `classes`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indexes for table `classtimes`
+--
+ALTER TABLE `classtimes`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indexes for table `sections`
+--
+ALTER TABLE `sections`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indexes for table `sectionstudents`
+--
+ALTER TABLE `sectionstudents`
+  ADD PRIMARY KEY (`SectionId`,`StudentId`);
+
+--
+-- Indexes for table `sectiontimes`
+--
+ALTER TABLE `sectiontimes`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `ux_academicid` (`AcademicId`) USING HASH,
+  ADD UNIQUE KEY `ux_email` (`Email`) USING HASH;
+
+--
+-- Indexes for table `weekdays`
+--
+ALTER TABLE `weekdays`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `attendances`
+--
+ALTER TABLE `attendances`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `classes`
+--
+ALTER TABLE `classes`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sections`
+--
+ALTER TABLE `sections`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+
+--
+-- AUTO_INCREMENT for table `sectiontimes`
+--
+ALTER TABLE `sectiontimes`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
